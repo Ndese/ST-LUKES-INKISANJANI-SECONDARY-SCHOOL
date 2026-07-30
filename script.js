78ff6d6500fcfccbffb4a59d244df3a2c2b6dd25
Kelvin Ndeseyia <ndeseyiakelvin@gmail.com>
2026-07-30 16:58:12 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function welcomeMessage() {
+    alert(
+    "Welcome to St. Luke's Inkisanjani School! Thank you for visiting our website."
+    );
+}
+
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert(
+"Thank you for contacting St. Luke's Inkisanjani School. We will get back to you soon."
+);
+
+this.reset();
+
+});
+
+document.getElementById("year").textContent =
+new Date().getFullYear();
+
+window.onload = function(){
+
+setTimeout(function(){
+
+alert(
+"Welcome to St. Luke's Inkisanjani School Website."
+);
+
+},1000);
+
+};
